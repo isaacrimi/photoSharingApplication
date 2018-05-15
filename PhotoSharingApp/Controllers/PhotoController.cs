21db55e818eae01916614f32cead0a8cab22ee2b
isaac <ishakrim1@gmail.com>
2018-05-15 17:12:43 +0100

    modif

--- PhotoSharingApp/Controllers/PhotoController.cs
+++ PhotoSharingApp/Controllers/PhotoController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PhotoSharingApp.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +11,8 @@ namespace PhotoSharingApp.Controller
     {   // GET: Photo
         public ActionResult Index()
         {
-            return View();
+            var photo = new Photo();
+            return View(photo);
         }
     }
 }
